8db5bc0b5d88706b8f0dd2edd24f601a794f337b
Elliot Barlas <elliot.barlas@logmein.com>
2020-11-14 13:35:01 -0800

    Add arrows svg file with separate layers for green up and red down arrows. Update rasterize Python script. Update wave.cpp to include scrolling buoy observation text in addition to wave animation.

--- wave.cpp
+++ wave.cpp
@@ -2,9 +2,12 @@
 #include <signal.h>
 #include <iostream>
 #include <fstream>
+#include <utility>
 #include <vector>
 #include <memory>
+#include <algorithm>
 #include "led-matrix.h"
+#include "graphics.h"
 
 uint8_t readByte(std::istream &is) {
     char t;
@@ -21,6 +24,10 @@ struct Rgb {
         r = readByte(is);
         g = readByte(is);
         b = readByte(is);
+    }
+
+    void render(rgb_matrix::FrameCanvas *buffer, int x, int y) const {
+        buffer->SetPixel(x, y, r, g, b);
     }
 };
 
@@ -40,12 +47,14 @@ struct Frame {
         }
     }
 
-    void render(rgb_matrix::FrameCanvas *buffer) {
+    void render(rgb_matrix::FrameCanvas *buffer, int left, int top) const {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int pos = y * width + x;
-                auto &pixel = pixels[pos];
-                buffer->SetPixel(x, y, pixel.r, pixel.g, pixel.b);
+                int xBuf = left + x;
+                int yBuf = top + y;
+                if (xBuf >= 0 && xBuf < buffer->width() && yBuf >= 0 && yBuf < buffer->height()) {
+                    pixels[y * width + x].render(buffer, xBuf, yBuf);
+                }
             }
         }
     }
@@ -63,12 +72,59 @@ struct Sprite {
         }
     }
 
-    static std::unique_ptr<Sprite> load(const char *file) {
-        auto ptr = std::make_unique<Sprite>();
+    static Sprite load(const char *file) {
+        Sprite sprite;
         std::ifstream is(file);
-        ptr->load(is);
+        sprite.load(is);
         is.close();
-        return ptr;
+        return sprite;
+    }
+};
+
+struct Animation {
+    virtual void init(rgb_matrix::FrameCanvas *buffer) = 0;
+    virtual int sleep() = 0;
+    virtual bool render(rgb_matrix::FrameCanvas *buffer) = 0;
+};
+
+struct Message {
+    rgb_matrix::Font *font;
+    rgb_matrix::Color *color;
+    std::string message;
+
+    int render(rgb_matrix::FrameCanvas *buffer, int x, int y) const {
+        return rgb_matrix::DrawText(
+                buffer,
+                *font,
+                x,
+                y + font->baseline(),
+                *color,
+                nullptr,
+                message.c_str(),
+                0);
+    }
+};
+
+struct ScrollingMessage : Animation {
+    Frame *frame;
+    Message message;
+    int left;
+
+    ScrollingMessage(Frame *frame, Message message) : frame(frame), message(std::move(message)) {}
+
+    void init(rgb_matrix::FrameCanvas *buffer) override {
+        left = buffer->width();
+    }
+
+    int sleep() override {
+        return 60;
+    }
+
+    bool render(rgb_matrix::FrameCanvas *buffer) override {
+        frame->render(buffer, left, 3);
+        int length = message.render(buffer, left + frame->width + 2, 0);
+        left--;
+        return left + frame->width + 2 + length < 0;
     }
 };
 
@@ -76,26 +132,6 @@ volatile bool interrupted = false;
 
 void interruptHandler(int signo) {
     interrupted = true;
-}
-
-void renderSprite(rgb_matrix::RGBMatrix *canvas, Sprite *wave, Sprite *fadeIn, Sprite *fadeOut) {
-    Sprite *sprites[] = {fadeIn, wave, fadeOut};
-    int loops[] = {1, 3, 1};
-    auto buffer = canvas->CreateFrameCanvas();
-    while (!interrupted) {
-        for (int i = 0; !interrupted && i < 3; i++) {
-            auto spr = sprites[i];
-            for (int j = 0; !interrupted && j < loops[i]; j++) {
-                for (int k = 0; !interrupted && k < spr->numFrames; k++) {
-                    auto &frame = spr->frames[k];
-                    buffer->Clear();
-                    frame.render(buffer);
-                    buffer = canvas->SwapOnVSync(buffer);
-                    usleep(60 * 1000);
-                }
-            }
-        }
-    }
 }
 
 rgb_matrix::RGBMatrix::Options makeOptions() {
@@ -106,27 +142,151 @@ rgb_matrix::RGBMatrix::Options makeOptions() {
     options.chain_length = 1;
     options.parallel = 1;
     options.show_refresh_rate = false;
-    options.limit_refresh_rate_hz = 60;
     options.brightness = 75;
     options.hardware_mapping = "classic-pi1";
     return options;
 }
 
+struct BuoyObs {
+    std::string message;
+    bool up;
+
+    static BuoyObs load(std::istream &is) {
+        std::string message;
+        std::getline(is, message);
+        return {message.substr(1), message[0] == '+'};
+    }
+
+    static std::vector<BuoyObs> load(const char *file) {
+        std::vector<BuoyObs> vec;
+        std::ifstream is(file);
+        while (is.good()) {
+            vec.push_back(load(is));
+        }
+        is.close();
+        return vec;
+    }
+};
+
+struct SpriteLoop {
+    Sprite *sprite;
+    int count;
+};
+
+struct SpriteAnimation : Animation {
+    std::vector<SpriteLoop> sprites;
+    int stage = 0;
+    int loop = 0;
+    int frame = 0;
+
+    void add(Sprite *sprite, int count) {
+        sprites.push_back({sprite, count});
+    }
+
+    void init(rgb_matrix::FrameCanvas *buffer) override {
+        stage = 0;
+    }
+
+    int sleep() override {
+        return 70;
+    }
+
+    bool render(rgb_matrix::FrameCanvas *buffer) override {
+        auto &spr = sprites[stage];
+        auto &sprFrame = spr.sprite->frames[frame];
+        sprFrame.render(buffer, 0, 0);
+        frame++;
+        // last sprite frame
+        if (frame == spr.sprite->numFrames) {
+            frame = 0;
+            loop++;
+            // last cycle
+            if (loop == spr.count) {
+                loop = 0;
+                stage++;
+                // last sprite
+                if (stage == static_cast<int>(sprites.size())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+void renderLoop(std::vector<Animation *> &animations, rgb_matrix::RGBMatrix *canvas, rgb_matrix::FrameCanvas *buffer) {
+    auto it = animations.begin();
+    (*it)->init(buffer);
+    while (!interrupted) {
+        buffer->Clear();
+        bool complete = (*it)->render(buffer);
+        buffer = canvas->SwapOnVSync(buffer);
+        if (complete) {
+            it++;
+            if (it == animations.end()) {
+                it = animations.begin();
+            }
+            (*it)->init(buffer);
+        }
+        usleep((*it)->sleep() * 1000);
+    }
+}
+
+std::vector<ScrollingMessage> makeMessages(
+        std::vector<BuoyObs> &observations,
+        rgb_matrix::Font &font,
+        rgb_matrix::Color &color,
+        Sprite &arrowsSprite) {
+    std::vector<ScrollingMessage> messages;
+    for (auto &obs : observations) {
+        auto &frame = arrowsSprite.frames[obs.up ? 0 : 1];
+        Message message{&font, &color, obs.message};
+        messages.emplace_back(&frame, message);
+    }
+    return messages;
+}
+
 int main(int argc, char *argv[]) {
     auto options = makeOptions();
+    rgb_matrix::RuntimeOptions runtimeOptions;
 
-    auto *canvas = rgb_matrix::RGBMatrix::CreateFromFlags(&argc, &argv, &options);
-    if (canvas == nullptr)
+    auto *canvas = rgb_matrix::RGBMatrix::CreateFromOptions(options, runtimeOptions);
+    if (canvas == nullptr) {
+        std::cerr << "Unable to create canvas" << std::endl;
         return 1;
+    }
+
+    rgb_matrix::Font font;
+    if (!font.LoadFont(argv[1])) {
+        std::cerr << "Unable to load font: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    rgb_matrix::Color color{0, 0, 255};
 
     auto waveSprite = Sprite::load("img/wave.bin");
     auto fadeInSprite = Sprite::load("img/fadein.bin");
     auto fadeOutSprite = Sprite::load("img/fadeout.bin");
+    auto arrowsSprite = Sprite::load("img/arrows.bin");
+
+    SpriteAnimation waveAnimation;
+    waveAnimation.add(&fadeInSprite, 1);
+    waveAnimation.add(&waveSprite, 3);
+    waveAnimation.add(&fadeOutSprite, 1);
+
+    std::vector<BuoyObs> observations = BuoyObs::load(argv[2]);
+    std::vector<ScrollingMessage> messages = makeMessages(observations, font, color, arrowsSprite);
 
     signal(SIGTERM, interruptHandler);
     signal(SIGINT, interruptHandler);
 
-    renderSprite(canvas, &*waveSprite, &*fadeInSprite, &*fadeOutSprite);
+    auto buffer = canvas->CreateFrameCanvas();
+
+    std::vector<Animation *> animations;
+    animations.push_back(&waveAnimation);
+    std::for_each(messages.begin(), messages.end(), [&animations](ScrollingMessage &m) { animations.push_back(&m); });
+
+    renderLoop(animations, canvas, buffer);
 
     canvas->Clear();
     delete canvas;
